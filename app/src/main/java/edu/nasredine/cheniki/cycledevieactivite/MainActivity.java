a07d79f06c5eec1cfbba64d9820ffc52a42ee75d
cheniki <nacer.cheniki@gmail.com>
2018-02-01 10:17:37 +0100

    Remplacer AppCompatActivity par Activity

--- app/src/main/java/edu/nasredine/cheniki/cycledevieactivite/MainActivity.java
+++ app/src/main/java/edu/nasredine/cheniki/cycledevieactivite/MainActivity.java
@@ -1,16 +1,16 @@
 package edu.nasredine.cheniki.cycledevieactivite;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
